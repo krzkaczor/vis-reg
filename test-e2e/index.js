a8cb60a1de9eb227b2474715af5d3e1415938d3a
Krzysztof Kaczor <chris@kaczor.io>
2017-10-02 11:14:06 +0200

    use fullscreenshooter package

--- test-e2e/index.js
+++ test-e2e/index.js
@@ -1,29 +1,31 @@
-const Nightmare = require("nightmare");
-const { screenshots } = require("./screenshots");
+const { delay } = require("bluebird");
+const puppeteer = require("puppeteer");
 const { join } = require("path");
+const { default: Fullscreenshot } = require('fullscreenshooter');
 
-async function main() {
-  const nightmare = new Nightmare({
-    show: false,
-  });
-  const URL = "file://" + join(__dirname, "../src/index.html");
-  await nightmare.goto(URL).wait("body");
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  await screenshots(nightmare, "index");
+  const basePath = join(process.cwd(), "screenshots");
+  const fullscreenshot = await Fullscreenshot.create({
+    basePath,
+    navbarOffset: 61,
+    unreveal: true,
+    widths: [350, 1500],
+    puppeteer: page
+  })
 
-  const t = await nightmare.evaluate(() => {
-    return document.title;
-  });
-  console.log("Title", t);
-  if (t !== "TESTPAGE"){
-    console.log("TITLE DOESNT MATCH!:", nightmare.title());
-    throw new Error("TITLE DOESNT MATCH!:")
-  }
+  const URL = "file://" + join(__dirname, "../src/index.html");  
+  await page.goto(URL);
+  console.log("Wait for page load");
+  delay(1000);
 
-  await nightmare.end();
-}
+  await fullscreenshot.save("index");
 
-main().catch((e) => {
+  await browser.close();
+  console.log("ALL DONE!");
+})().catch((e) => {
   console.error(e);
   process.exit(1);
 });
